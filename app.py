30a33c658c619dfcdc24f4b389211c6844600d83
Ted Hynes <tedhynes@Teds-MacBook-Air.local>
2026-05-14 13:57:38 +0100

    Adds Docker containerisation support

--- app.py
+++ app.py
@@ -184,4 +184,4 @@ def logout():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
